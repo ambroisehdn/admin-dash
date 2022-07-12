f09827b03c1acba139275d9895e68a8df71d16d9
ambroisehdn <ambroise.hound@gmail.com>
2022-07-12 06:40:26 +0100

    in memory auth add with bcryp encrypt

--- src/main/java/com/yieldigit/authapp/AuthappApplication.java
+++ src/main/java/com/yieldigit/authapp/AuthappApplication.java
@@ -2,12 +2,43 @@ package com.yieldigit.authapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@EnableWebSecurity
 @SpringBootApplication
-public class AuthappApplication {
+public class AuthappApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthappApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder bcryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		encoder.encode("password");
+		auth.inMemoryAuthentication()
+			.withUser("user").password(encoder.encode("password")).roles("USER")
+			.and()
+				.withUser("admin").password(encoder.encode("password")).roles("ADMIN");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeHttpRequests()
+					.antMatchers("/home").permitAll()
+					.antMatchers("/admin").hasRole("ADMIN")
+				.anyRequest().authenticated().and().formLogin();
+	}
 }
--- src/main/java/com/yieldigit/authapp/controller/HomeController.java
+++ src/main/java/com/yieldigit/authapp/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.yieldigit.authapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
     
-    @GetMapping("/")
+    @GetMapping(value = "home")
     public String home() {
         return "Hello World!";
+    }
+
+    @GetMapping(value = "admin")
+    public String admin() {
+        return "Hello Admin!";
     }
 }
